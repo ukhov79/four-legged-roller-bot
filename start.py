b5f0eb631d5af8ae2936cc6c9bb20a120ea8338e
Peter A. Ukhov <38152650+ukhov79@users.noreply.github.com>
2021-02-22 11:37:58 +0300

    Update start.py

--- start.py
+++ start.py
@@ -40,7 +40,7 @@ def PowerOnReset(bus):
 script, ip = argv
 # Инициализация i2c 
 # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(0)
 PowerOnReset(bus)
 QuickStart(bus)
 
